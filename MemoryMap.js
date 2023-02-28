acd87a9618bdc8564b5c9d897be62590cd2a0674
Ashok Kushwaha <106888682+ashokkushh@users.noreply.github.com>
2023-02-28 23:15:47 +0530

    Update MemoryMap.js
    JS UPDATE

--- MemoryMap.js
+++ MemoryMap.js
@@ -4,7 +4,7 @@ var locValue = document.getElementById("disp1");
 let sw = 0;
 memloc.value = "DEI 8085";
 const memory = [];
-for (let i = 0; i <= 10; i++) {
+for (let i = 0; i < 10; i++) {
     memory[i] = 0;
 }
 for (let i = 10; i <= 65535; i++) {
@@ -21,8 +21,14 @@ function hexToDec(hex) {
  }
 
 function nextbtn() {
-    let locationInDec = hexToDec(memloc.value);
+    let locationInDec ;
+   if(sw == 2){
+    locationInDec = hexToDec(memloc.value);
     locValue.value = memory[locationInDec];
+   } else{
+   locationInDec = hexToDec(memloc.value);
+   locValue.value = memory[locationInDec+1];
+}
   //  locValue.value = memory[memloc.value];
     sw++;
     if(sw%2 == 0){
@@ -77,103 +83,229 @@ function fillbtn(){
 }
 let SP;
 function  processor(start_index){
-    for(let i =start_index;i<memory.length;i++){
+ // A B C D E H L SP ---> 
+ // 0 1 2 3 4 5 6 7 8 9
+let start_index1 = hexToDec(start_index);
+    for(let i =start_index1;i<memory.length;i++){
         if(memory[i] == "76"){
+            //HLT
             return;
         }
-    
-      //System.out.print(memory[i]+" ");
       if(memory[i] == "3A"){
-        //LAD
-          memory[0] = memory[++i];
-          SP =0;
-       // System.out.println("yessssss3A  " + memory[0]);
-
-
-       }
+        //LDA
+            let index = hexToDec(memory[i+2] + memory[i+1]);
+            memory[0] = memory[index];
+            SP =0;
+            i = i+2;
+            continue;
+        }
        if(memory[i] == "6F"){
         // mov l,a
         memory[6] = memory[0];
         SP = 6;
-       // System.out.println("yessssss6F  " + memory[6]);
-
+       continue;
     }
     if(memory[i] == "01"){
-        //LXI B  (problem of register pair!!!!!!!!   b = b+c)
+        // LXI B, add
+        memory[2] = memory[++i];
         memory[1] = memory[++i];
+
+        continue;
         //pointer
-        SP =1;
-       // System.out.println("yessssss01  " + memory[1] );
+        SP = 1;
 
     }
     if(memory[i] == "4D"){
-        memory[2] = memory[6];
-        
+        // MOV C,L
+        memory[2] = memory[6]; 
         SP = 2;
-       // System.out.println("yessssss4D  " + memory[2]);
-
+       continue;
     }
     if(memory[i] == "0D"){
         //DCR C
-        memory[2] = String.valueOf(Integer.valueOf(memory[2])-1);
+        let temp = hexToDec(memory[2]);
+        memory[2] = dectoHex(temp-1);
         SP = 2;
-        memory[1] = String.valueOf(Integer.valueOf(memory[1])+Integer.valueOf(memory[2]));
-       // System.out.println("yessssss0D  " + memory[1]);
-
+       continue;
     }
     if(memory[i] == "11"){
-        //LXI D (location of destination)
+        //LXI D
+        memory[4] = memory[++i];
         memory[3] = memory[++i];
-        //System.out.println(memory[3]);
         SP =3;
-       // System.out.println("yessssss11  " + memory[3]);
-        
+       continue;
     }
     if(memory[i] == "0A"){
-       // LDAX B
-        memory[0] = memory[1];
+       // LDAX B    LOAD THE CONTENTS OF LOACTION "WHICH IS STORED"IN BC PAIR 
+         let address0 =memory[2] ; //---->C
+         let address1 =memory[1] ; //---->B
+         let address = hexToDec(address1)*16*16 +hexToDec(address0 ) ;
+        memory[0] = memory[address];
         SP = 0;
-       // System.out.println("yessssss0A  " + memory[0]);
-
+       continue;
     }
     if(memory[i] == "12"){
-        //STAX D
-        memory[memory[3]] = memory[0];
-        SP = [3];
-      //  System.out.println("yessssss12  " + SP + " " + memory[0]);
-
+        let address0 =memory[4] ; //---->C
+        let address1 =memory[3] ; //---->B
+        let address = hexToDec(address1)*16*16 +hexToDec(address0 ) ;
+        //STAX D   STORE A IN MEMORY LOCATION POINTED BY DE REGISTER PAIR
+        memory[address] = memory[0];
+        SP = 3;
+      continue;
     }
     if(memory[i] == "0B"){
         //DCX B
-        memory[1] = memory[1]-1;
+      let address0 =memory[2] ; //---->C
+      let address1 =memory[1] ; //---->B
+      let address = hexToDec(address1)*16*16 +hexToDec(address0 ) ;
+        address--;
+        memory[1] = dectoHex(parseInt(address/256, 10));
+        memory[2] = dectoHex(address % 256);
        SP = 1;
-      // System.out.println("yessssss0B  "+memory[1]);
-
     }
     if(memory[i] == "13"){
         //INX D
-        memory[3] = memory[3]+1;
-      SP = 3;
-     // System.out.println("yessssss13  "+ memory[3]);
-
-        
+      let address0 =memory[4] ; //---->E
+      let address1 =memory[3] ; //---->D
+      let address = hexToDec(address1)*16*16 +hexToDec(address0 ) ;
+        address++;
+        memory[3] = dectoHex(parseInt(address/256, 10));
+        memory[4] = dectoHex(address % 256);
+       SP =3; 
     }
     if(memory[i] == "2D"){
         //DCR L
-        memory[6] = memory[6]-1;
+        memory[6] = dectoHex(hexToDec(memory[6])-1);
        SP = 6;
-      // System.out.println("yessssss2D  " + memory[6]);
-
-        
     }
     if(memory[i] == "C2"){
+        // JNZ add
+        let address0 =memory[++i] ; //---->C
+        let address1 =memory[++i] ; //---->B
+        let address = hexToDec(address1)*16*16 +hexToDec(address0 ) ;
         if(memory[SP] != 0){
-            i = memory[i+1]-1+10;
+            i = address-1;
+            continue;
         }
-      //  System.out.println("yessssssC2  "+ i);
-
     }  
-    
+    //------------------------------------------------
+    if(memory[i]=="47"){
+        // MOV B,A
+        memory[1] = memory[0];
+        SP = 1;
+        continue;
+    }
+
+    if(memory[i]=="4F"){
+        // MOV C,A
+        memory[2] = memory[0];
+        SP = 2;
+        continue;
+    }
+
+    if(memory[i]=="78"){
+        // MOV A.B
+        memory[0] = memory[1];
+        SP = 0;
+        continue;
+    }
+
+    if(memory[i]=="32"){
+        // STA ADDR
+        let ind = hexToDec(memory[i+2] + memory[i+1]);
+
+        memory[ind] = memory[0];
+        SP = ind;
+        i=i+2;
+        continue;
+    }
+
+    if(memory[i]=="01"){
+        // LXI B
+        memory[2] = memory[i+1];
+        memory[1] = memory[i+2];
+        i = i+2;
+        SP = 1;
+        continue;
+    }
+
+    if(memory[i]=="11"){
+        // LXI D
+        memory[4] = memory[i+1];
+        memory[3] = memory[i+2];
+        i = i+2;
+        SP = 4;
+        continue;
+    }
+
+    if(memory[i]=="21"){
+        // LXI H
+        memory[6] = memory[i+1];
+        memory[5] = memory[i+2];
+        i = i+2;
+        SP = 5;
+        continue;
+    }
+    if(memory[i]=="26"){
+        // MVI H,data
+        memory[5] = memory[++i];
+        SP = 5;
+        continue;
+    }
+    if(memory[i]=="3E"){
+        // MVI A
+        memory[0] = memory[++i];
+        SP = 0;
+        continue;
+    }
+    if(memory[i]=="06"){
+        // MVI B
+        memory[1] = memory[++i];
+        SP = 1;
+        continue;
+    }
+    if(memory[i]=="0E"){
+        // MVI C
+        memory[2] = memory[++i];
+        SP = 2;
+        continue;
+    }
+    if(memory[i]=="16"){
+        // MVI D
+        memory[3] = memory[++i];
+        SP = 3;
+        continue;
+    }
+    if(memory[i]=="1E"){
+        // MVI E
+        memory[4] = memory[++i];
+        SP = 4;
+        continue;
+    }
+    if(memory[i]=="2E"){
+        // MVI L
+        memory[6] = memory[++i];
+        SP = 6;
+        continue;
+    }
+    if(memory[i] == "25"){
+        //DCR H
+            let v = hexToDec(memory[5]);
+            memory[5] = dectoHex(v-1);
+            SP = 6;
+            continue;
+    }
+    if(memory[i] == "03"){
+        //INX B
+      let address0 =memory[2] ; //---->E
+      let address1 =memory[1] ; //---->D
+      let address = hexToDec(address1)*16*16 +hexToDec(address0 ) ;
+        address++;
+        memory[1] = dectoHex(parseInt(address/256, 10));
+        memory[2] = dectoHex(address % 256);
+        SP = 1;  
+    }
+ 
    
    }
 
